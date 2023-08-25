311538f5066b38863e997c0aff5b741992330f37
David M. Perlman <omgoleus@gmail.com>
2023-08-25 07:55:05 -0700

    Code up to and including first test run

--- answer_questions.py
+++ answer_questions.py
@@ -1,34 +1,68 @@
 import openai
 import json
-import numpy as np
 import textwrap
+import xml.etree.ElementTree as ET
+import sys
 import re
-from time import time,sleep
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import os.path
+import yaml
+import time
+from time import time, sleep
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+    retry_if_not_exception_type
+)  # for exponential backoff
 
+# TO DO
+# Add full logging
+# Improve prompts
+# of course add CWE and CPE
+# Finally add Leviathan data
 
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
 
 
-openai.api_key = open_file('openaiapikey.txt')
+openai.api_key = open_file('openaiapikey_stanford.txt')
 
 
-def gpt3_embedding(content, engine='text-similarity-ada-001'):
-    content = content.encode(encoding='ASCII',errors='ignore').decode()
-    response = openai.Embedding.create(input=content,engine=engine)
+def retryCallback(retry_state):
+    print(retry_state.outcome)
+    return 'EMBED_FAIL'
+
+@retry(wait=wait_random_exponential(min=3, max=120), 
+       stop=stop_after_attempt(6), 
+       retry=retry_if_not_exception_type(openai.error.InvalidRequestError),
+       retry_error_callback=retryCallback)
+def gpt3_embedding(content, engine='text-embedding-ada-002'):
+    # V2 embeddings can have 8191 tokens in input
+    try:
+        response = openai.Embedding.create(input=content,engine=engine)
+    except openai.error.InvalidRequestError:
+        return 'invalidRequest'
     vector = response['data'][0]['embedding']  # this is a normal list
+    #print(vector)
     return vector
 
 
+
 def similarity(v1, v2):  # return dot product of two vectors
+    if isinstance(v1, str) or isinstance(v2,str):
+        return 0
     return np.dot(v1, v2)
 
 
 def search_index(text, data, count=20):
     vector = gpt3_embedding(text)
     scores = list()
-    for i in data:
+    print('calculating all embedding similarities...')
+    for i in tqdm(data):
         score = similarity(vector, i['vector'])
         #print(score)
         scores.append({'content': i['content'], 'score': score})
@@ -36,22 +70,28 @@ def search_index(text, data, count=20):
     return ordered[0:count]
 
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.6, top_p=1.0, tokens=2000, freq_pen=0.25, pres_pen=0.0, stop=['<<END>>']):
+# use either gpt-4-32k
+# or gpt-3.5-turbo-16k
+def gpt3_completion(prompt, model='gpt-3.5-turbo-16k', temp=0.6, top_p=1.0, tokens=2000, freq_pen=0.25, pres_pen=0.0, stop=['<<END>>']):
     max_retry = 5
     retry = 0
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
     while True:
         try:
-            response = openai.Completion.create(
-                engine=engine,
-                prompt=prompt,
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages = [
+                    {"role":"user", "content":prompt}
+                ],
                 temperature=temp,
                 max_tokens=tokens,
                 top_p=top_p,
                 frequency_penalty=freq_pen,
                 presence_penalty=pres_pen,
                 stop=stop)
-            text = response['choices'][0]['text'].strip()
+            print(response)
+            #text = response['choices'][0]['text'].strip()
+            text = response['choices'][0]['message']['content'].strip()
             text = re.sub('\s+', ' ', text)
             filename = '%s_gpt3.txt' % time()
             with open('gpt3_logs/%s' % filename, 'w') as outfile:
@@ -66,7 +106,7 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.6, top_p=1.0, toke
 
 
 if __name__ == '__main__':
-    with open('index.json', 'r') as infile:
+    with open('nvdcve_embedding.json', 'r') as infile:
         data = json.load(infile)
     #print(data)
     while True:
@@ -77,7 +117,8 @@ if __name__ == '__main__':
         #exit(0)
         answers = list()
         # answer the same question for all returned chunks
-        for result in results:
+        print('creating sub-answers for each relevant chunk')
+        for result in tqdm(results):
             prompt = open_file('prompt_answer.txt').replace('<<PASSAGE>>', result['content']).replace('<<QUERY>>', query)
             answer = gpt3_completion(prompt)
             print('\n\n', answer)
@@ -87,7 +128,10 @@ if __name__ == '__main__':
         chunks = textwrap.wrap(all_answers, 10000)
         final = list()
         for chunk in chunks:
-            prompt = open_file('prompt_summary.txt').replace('<<SUMMARY>>', chunk)
+            prompt = open_file('prompt_summary.txt').replace('<<SUMMARY>>', chunk).replace('<<QUERY>>', query)
             summary = gpt3_completion(prompt)
             final.append(summary)
         print('\n\n=========\n\n', '\n\n'.join(final))
+
+# test query:
+# We have a system based on Windows 3.1 that has been upgraded with a TCP-IP stack from 2012 and we want to know if it has any DNS-related problems we should be worried about.
---
+++ nvdcve_embedding.py
@@ -0,0 +1,203 @@
+import openai
+import json
+import textwrap
+import xml.etree.ElementTree as ET
+import sys
+import re
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import os.path
+import yaml
+import time
+from time import time, sleep
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+    retry_if_not_exception_type
+)  # for exponential backoff
+
+
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return infile.read()
+
+
+#openai.api_key = open_file('openaiapikey_leviathan.txt')
+openai.api_key = open_file('openaiapikey_stanford.txt')
+
+
+def retryCallback(retry_state):
+    print(retry_state.outcome)
+    return 'EMBED_FAIL'
+
+@retry(wait=wait_random_exponential(min=3, max=120), 
+       stop=stop_after_attempt(6), 
+       retry=retry_if_not_exception_type(openai.error.InvalidRequestError),
+       retry_error_callback=retryCallback)
+def gpt3_embedding(content, model='text-embedding-ada-002'):
+    # V2 embeddings can have 8191 tokens in input
+    try:
+        response = openai.Embedding.create(input=content, model=model)
+    except openai.error.InvalidRequestError:
+        return 'invalidRequest'
+    vector = response['data'][0]['embedding']  # this is a normal list
+    return vector
+
+def load_nvdcve():
+    ftemp = 'nvdcvecwecpedata/nvdcve-1.1-%d.json'
+    years = [2002]
+    years = range(2002, 2024) # last one is not included so it goes to 2023
+    nvdcve = []
+    for i in years:
+        fname = ftemp % i
+        print('Loading %s' % fname)
+        with open(fname, 'r') as infile:
+            data = json.load(infile)["CVE_Items"]
+        nvdcve.extend(data)
+    return nvdcve
+
+def parse_nvdcve(nvdcve):
+    out = []
+    print('Parsing data')
+    impacts = 0
+    noImpacts = 0
+    for i in tqdm(nvdcve):
+        proc = parse_nvdcve_item(i)
+        if not proc: continue
+        out.append(proc)
+        if proc['severity'] == 'unknown':
+            noImpacts += 1
+        else:
+            impacts += 1
+    print('Total complete data: %d Missing impacts: %d' % (impacts, noImpacts))
+    # 209412, missing: 503
+    return out
+    
+
+def parse_nvdcve_item(cve):
+    cveID = cve['cve']["CVE_data_meta"]["ID"]
+    cwe = cve['cve']["problemtype"]["problemtype_data"][0]["description"]
+    cwe = [i['value'] for i in cwe]
+    description = cve['cve']["description"]["description_data"]
+    description = '\n\n'.join([i['value'].replace('\n', ' ') for i in description])
+    if description.startswith('** REJECT **'):
+        return False;
+    cpes = parse_nvdcve_configurations(cve["configurations"])
+    # now do impact etc
+    #print(cve)
+    severity, exploitability, impact = parse_nvdcve_impacts(cve['impact'])
+    if severity == 'unknown':
+        print(cve)
+    out = {'cve':cveID, 'cwes':cwe, 'cpes':cpes, 'severity':severity, 'exploitability':exploitability, 'impact':impact, 'description':description}
+    return out
+
+
+def parse_nvdcve_configurations(c):
+    nodes = c['nodes']
+    out = []
+    for n in nodes:
+        cpe23Uris = [i["cpe23Uri"] for i in n["cpe_match"]]
+        cpes = [' '.join(i.split(':')[3:5]) for i in cpe23Uris]
+        cpes = list(set(cpes)) # remove duplicates
+        out.extend(cpes)
+    return out
+
+def parse_nvdcve_impacts(i):
+    #print(i)
+    if "baseMetricV2" in i:
+        severity = i["baseMetricV2"]['severity']
+        exploitability = float(i["baseMetricV2"]["exploitabilityScore"])/2
+        impact = float(i["baseMetricV2"]["impactScore"])/2
+    elif "baseMetricV3" in i:
+        severity = i["baseMetricV3"]["cvssV3"]["baseSeverity"]
+        exploitability = float(i["baseMetricV3"]["exploitabilityScore"])
+        impact = float(i["baseMetricV3"]["impactScore"])
+    else:
+        print(i)
+        severity = 'unknown'
+        exploitability = 'unknown'
+        impact = 'unknown'
+    return severity, exploitability, impact
+
+def chunk_yaml_list(input, name, size=2000):
+    fname = '%s_yaml_chunks_size_%s.json' % (name, size)
+    #input = input[0:500]
+    if os.path.isfile(fname):
+        print('reading already chunked yaml of size %d' % size)
+        with open(fname, 'r') as infile:
+            out = json.load(infile)
+    else:
+        out = []
+        outItem = ''
+        outItemLen = 0
+        print('chunking yaml output to size %d' % size)
+        for i in tqdm(input):
+            y = yaml.safe_dump(i, sort_keys=False, width=size)
+            l = len(y)
+            #print(l)
+            if outItemLen + l + 2 > size:
+                out.append(outItem)
+                outItem = y
+                outItemLen = l
+            else:
+                outItem += ('\n\n' + y)
+                outItemLen += (2 + l)
+        with open(fname, 'w') as outfile:
+            json.dump(out, outfile, indent=2)
+    return out
+
+
+def embed_text_list(l):
+    out = []
+    #l = l[0:3]
+    n = 0
+    for i in tqdm(l):
+        embedding = gpt3_embedding(i.encode(encoding='ASCII',errors='ignore').decode())
+        if embedding == 'EMBED_FAIL':
+            print('embed fail for item number %d of %d' % (n, len(l)))
+            time.sleep(300)
+        info = {'content': i, 'vector': embedding}
+        #print(f_vect(embedding), '\n\n\n')
+        out.append(info)
+        n += 1
+    return out
+
+def f_vect(v):
+    return ', '.join('%0.3f' % x for x in v)
+
+
+if __name__ == '__main__':
+    nvdcvefile = 'nvdcve.json'
+    if os.path.isfile(nvdcvefile):
+        with open(nvdcvefile, 'r') as infile:
+            nvdcve = json.load(infile)
+    else:
+        nvdcve = parse_nvdcve(load_nvdcve())
+        with open(nvdcvefile, 'w') as outfile:
+            json.dump(nvdcve, outfile, indent=2)
+    # now we have nvdcve, one way or another
+    # need to break into max 8192 token chunks
+    chunksize = 2000
+    chunkList = chunk_yaml_list(nvdcve, 'nvdcve', size=chunksize)
+    print("Number of chunks: %d" % len(chunkList))
+    # that works. now try the model
+    nvdcve_embedding = embed_text_list(chunkList)
+    with open('nvdcve_embedding_%d.json' % chunksize, 'w') as outfile:
+        json.dump(nvdcve_embedding, outfile, indent=2)
+
+    sys.exit()
+
+
+    ########## OK GREAT!!!
+    # I did nvdcve
+    # now run the AI thing on it
+    # in order to make 
+
+
+    # Then after that,
+    # do CWE and then CPE
+    #alltext = open_file('nvdcvecwecpedata/allitems.xml')
+    #tree = ET.parse('nvdcvecwecpedata/cwec_v4.12.xml') 
+    #root = tree.getroot()
